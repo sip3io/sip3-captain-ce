f087dc07577641b6d68bb396dd3e264c391b37b4
agafox <agafox@sip3.io>
2020-06-20 16:30:21 +0300

    Added: Optional payload compression

--- src/test/kotlin/io/sip3/captain/ce/encoder/EncoderTest.kt
+++ src/test/kotlin/io/sip3/captain/ce/encoder/EncoderTest.kt
@@ -23,10 +23,13 @@ import io.sip3.commons.domain.payload.ByteArrayPayload
 import io.sip3.commons.vertx.test.VertxTest
 import io.sip3.commons.vertx.util.localRequest
 import io.vertx.core.buffer.Buffer
+import io.vertx.core.json.JsonObject
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
 import java.sql.Timestamp
+import java.util.zip.InflaterInputStream
 
 class EncoderTest : VertxTest() {
 
@@ -37,10 +40,29 @@ class EncoderTest : VertxTest() {
                 0x03.toByte(), 0x03.toByte(), 0x79.toByte(), 0xc4.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
                 0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
                 0x00.toByte(), 0x00.toByte())
+
+        // Payload: ICMP
+        val PACKET_2 = byteArrayOf(
+                0x03.toByte(), 0x03.toByte(), 0x79.toByte(), 0xc4.toByte(), 0x00.toByte(), 0x00.toByte(), 0x00.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x45.toByte(), 0xb8.toByte(), 0x00.toByte(), 0xc8.toByte(), 0xbf.toByte(), 0xd3.toByte(),
+                0x00.toByte(), 0x00.toByte())
     }
 
     @Test
-    fun `Encode ICMP`() {
+    fun `Encode not compressed ICMP`() {
         val timestamp = Timestamp(System.currentTimeMillis())
         val srcAddr = byteArrayOf(0x0a.toByte(), 0xfa.toByte(), 0xf4.toByte(), 0x05.toByte())
         val dstAddr = byteArrayOf(0x0a.toByte(), 0xfa.toByte(), 0xf4.toByte(), 0x06.toByte())
@@ -113,6 +135,96 @@ class EncoderTest : VertxTest() {
                             assertEquals(8, buffer.getByte(55))
                             assertEquals(3 + PACKET_1.size, buffer.getShort(56).toInt())
                             assertArrayEquals(PACKET_1, buffer.getBytes(58, 58 + PACKET_1.size))
+                        }
+                        context.completeNow()
+                    }
+                }
+        )
+    }
+
+    @Test
+    fun `Encode compressed ICMP`() {
+        val timestamp = Timestamp(System.currentTimeMillis())
+        val srcAddr = byteArrayOf(0x0a.toByte(), 0xfa.toByte(), 0xf4.toByte(), 0x05.toByte())
+        val dstAddr = byteArrayOf(0x0a.toByte(), 0xfa.toByte(), 0xf4.toByte(), 0x06.toByte())
+        val srcPort = 5060
+        val dstPort = 5061
+        runTest(
+                deploy = {
+                    vertx.deployTestVerticle(Encoder::class,
+                            config = JsonObject().apply {
+                                put("encoder", JsonObject().apply {
+                                    put("min-size-to-compress", PACKET_2.size)
+                                })
+                            })
+                },
+                execute = {
+                    val packet = Packet().apply {
+                        this.timestamp = timestamp
+                        this.srcAddr = srcAddr
+                        this.dstAddr = dstAddr
+                        this.srcPort = srcPort
+                        this.dstPort = dstPort
+                        protocolCode = PacketTypes.ICMP
+                        payload = ByteArrayPayload(PACKET_2)
+                    }
+                    vertx.eventBus().localRequest<Any>(RoutesCE.encoder, listOf(packet))
+                },
+                assert = {
+                    vertx.eventBus().consumer<List<Buffer>>(RoutesCE.sender) { event ->
+                        val buffers = event.body()
+                        context.verify {
+                            assertEquals(1, buffers.size)
+
+                            val buffer = buffers[0]
+
+                            assertEquals(82, buffer.length())
+                            // Prefix
+                            assertArrayEquals(Encoder.PREFIX, buffer.getBytes(0, 4))
+                            // Compressed
+                            assertEquals(1, buffer.getByte(4))
+                            // Type
+                            assertEquals(1, buffer.getByte(5))
+                            // Version
+                            assertEquals(1, buffer.getByte(6))
+                            // Length
+                            assertEquals(77, buffer.getShort(7))
+                            // Milliseconds
+                            assertEquals(1, buffer.getByte(9))
+                            assertEquals(11, buffer.getShort(10))
+                            assertEquals(timestamp.time, buffer.getLong(12))
+                            // Nanoseconds
+                            assertEquals(2, buffer.getByte(20))
+                            assertEquals(7, buffer.getShort(21))
+                            assertEquals(timestamp.nanos, buffer.getInt(23))
+                            // Source Address
+                            assertEquals(3, buffer.getByte(27))
+                            assertEquals(3 + srcAddr.size, buffer.getShort(28).toInt())
+                            assertArrayEquals(srcAddr, buffer.getBytes(30, 30 + srcAddr.size))
+                            // Destination Address
+                            assertEquals(4, buffer.getByte(34))
+                            assertEquals(3 + dstAddr.size, buffer.getShort(35).toInt())
+                            assertArrayEquals(dstAddr, buffer.getBytes(37, 37 + dstAddr.size))
+                            // Source Port
+                            assertEquals(5, buffer.getByte(41))
+                            assertEquals(5, buffer.getShort(42))
+                            assertEquals(srcPort, buffer.getShort(44).toInt())
+                            // Destination Port
+                            assertEquals(6, buffer.getByte(46))
+                            assertEquals(5, buffer.getShort(47))
+                            assertEquals(dstPort, buffer.getShort(49).toInt())
+                            // Protocol Code
+                            assertEquals(7, buffer.getByte(51))
+                            assertEquals(4, buffer.getShort(52))
+                            assertEquals(PacketTypes.ICMP, buffer.getByte(54))
+                            // Payload
+                            assertEquals(8, buffer.getByte(55))
+                            assertEquals(27, buffer.getShort(56).toInt())
+                            var payload = buffer.getBytes(58, 58 + 24)
+                            InflaterInputStream(ByteArrayInputStream(payload)).use {
+                                payload = it.readBytes()
+                            }
+                            assertArrayEquals(PACKET_2, payload)
                         }
                         context.completeNow()
                     }
