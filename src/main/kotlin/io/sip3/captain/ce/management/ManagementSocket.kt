ce4c3ec05f4676c37e27e6238bdfc7b33a72cd9d
Aleksandr <leo@sip3.io>
2024-07-04 22:22:11 +0700

    Added: WebSocket and TCP support for Sender and ManagementSocket

--- src/main/kotlin/io/sip3/captain/ce/management/ManagementSocket.kt
+++ src/main/kotlin/io/sip3/captain/ce/management/ManagementSocket.kt
@@ -16,6 +16,7 @@
 
 package io.sip3.captain.ce.management
 
+import io.netty.buffer.ByteBufUtil
 import io.sip3.captain.ce.RoutesCE
 import io.sip3.commons.domain.media.MediaControl
 import io.sip3.commons.vertx.annotations.ConditionalOnProperty
@@ -23,9 +24,13 @@ import io.sip3.commons.vertx.annotations.Instance
 import io.sip3.commons.vertx.util.closeAndExitProcess
 import io.sip3.commons.vertx.util.localPublish
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.buffer.Buffer
 import io.vertx.core.datagram.DatagramSocket
 import io.vertx.core.datagram.DatagramSocketOptions
+import io.vertx.core.http.WebSocket
 import io.vertx.core.json.JsonObject
+import io.vertx.core.net.NetSocket
+import io.vertx.core.parsetools.RecordParser
 import mu.KotlinLogging
 import java.net.URI
 import java.nio.charset.Charset
@@ -35,7 +40,7 @@ import java.nio.charset.Charset
  */
 @Instance(singleton = true)
 @ConditionalOnProperty("/management")
-class ManagementSocket : AbstractVerticle() {
+open class ManagementSocket : AbstractVerticle() {
 
     private val logger = KotlinLogging.logger {}
 
@@ -48,23 +53,44 @@ class ManagementSocket : AbstractVerticle() {
     }
 
     lateinit var uri: URI
-    private var registerDelay: Long = 60000
+    var registerDelay: Long = 60000
+    var reconnectionTimeout: Long? = null
+    var delimiter = "\r\n\r\n3PIS\r\n\r\n"
 
     private var udp: DatagramSocket? = null
+    private var tcp: NetSocket? = null
+    protected var ws: WebSocket? = null
 
     override fun start() {
         config().getJsonObject("management").let { config ->
             uri = URI(config.getString("uri") ?: throw IllegalArgumentException("uri"))
             config.getLong("register_delay")?.let { registerDelay = it }
+            config.getLong("reconnection_timeout")?.let { reconnectionTimeout = it}
+            config.getString("delimiter")?.let { delimiter = it }
         }
 
         when (uri.scheme) {
-            "udp" -> startUdpSocket()
+            "udp" -> openUdpConnection()
+            "tcp" -> openTcpConnection()
+            "ws", "wss" -> openWsConnection()
             else -> throw NotImplementedError("Unknown protocol: $uri")
         }
     }
 
-    private fun startUdpSocket() {
+    open fun sendRegister(send: ((Buffer) -> Unit)) {
+            val registerMessage = JsonObject().apply {
+                put("type", TYPE_REGISTER)
+                put("payload", JsonObject().apply {
+                    put("timestamp", System.currentTimeMillis())
+                    put("deployment_id", deploymentID())
+                    put("config", config())
+                })
+            }
+
+            send(registerMessage.toBuffer())
+    }
+
+    private fun openUdpConnection() {
         val options = DatagramSocketOptions().apply {
             isIpV6 = uri.host.matches(Regex("\\[.*]"))
         }
@@ -79,21 +105,59 @@ class ManagementSocket : AbstractVerticle() {
         }
 
         vertx.setPeriodic(0, registerDelay) {
-            val registerMessage = JsonObject().apply {
-                put("type", TYPE_REGISTER)
-                put("payload", JsonObject().apply {
-                    put("timestamp", System.currentTimeMillis())
-                    put("deployment_id", deploymentID())
-                    put("config", config())
-                })
+            sendRegister { buffer ->
+                udp?.send(buffer, uri.port, uri.host)
+                    ?.onFailure { logger.error(it) { "DatagramSocket 'send()' failed." } }
             }
-
-            udp?.send(registerMessage.toBuffer(), uri.port, uri.host)
-                ?.onFailure { logger.error(it) { "DatagramSocket 'send()' failed." } }
         }
     }
 
-    private fun handle(message: JsonObject) {
+    private fun openTcpConnection() {
+        vertx.createNetClient().connect(uri.port, uri.host)
+            .onFailure { t ->
+                logger.error(t) { "Failed to connect to $uri" }
+                reconnectionTimeout?.let { timeout ->
+                    vertx.setTimer(timeout) { openTcpConnection() }
+                }
+            }
+            .onSuccess { socket ->
+                val parser = RecordParser.newDelimited(delimiter) { buffer ->
+                    try {
+                        val message = buffer.toJsonObject()
+                        handle(message)
+                    } catch (e: Exception) {
+                        logger.error("ManagementSocket 'handle()' failed. Message: ${buffer.toString(Charset.defaultCharset())}", e)
+                    }
+                }
+
+                tcp = socket.handler { buffer ->
+                    try {
+                        parser.handle(buffer)
+                    } catch (e: Exception) {
+                        logger.error(e) { "RecordParser 'handle()' failed." }
+                        logger.debug { "Sender: $uri, buffer: ${ByteBufUtil.prettyHexDump(buffer.byteBuf)}" }
+                    }
+                }.closeHandler {
+                    logger.info("TCP connection closed: $uri")
+                    reconnectionTimeout?.let { timeout ->
+                        vertx.setTimer(timeout) { openTcpConnection() }
+                    }
+                }
+
+                vertx.setPeriodic(0, registerDelay) {
+                    sendRegister { buffer ->
+                        tcp?.write(buffer.appendString(delimiter))
+                            ?.onFailure { logger.error(it) { "NetSocket 'write()' failed." } }
+                    }
+                }
+            }
+    }
+
+    open fun openWsConnection() {
+        throw NotImplementedError("WebSocket transport is available in EE version")
+    }
+
+    open fun handle(message: JsonObject) {
         val type = message.getString("type")
         val payload = message.getJsonObject("payload")
 
@@ -121,7 +185,9 @@ class ManagementSocket : AbstractVerticle() {
                 vertx.eventBus().localPublish(RoutesCE.media + "_recording_reset", payload)
             }
 
-            else -> logger.error("Unknown message type '$type'. Message: ${message.encodePrettily()}")
+            else -> logger.debug {
+                "Unknown message type '$type'. Message: ${message.encodePrettily()}"
+            }
         }
     }
 }
